52a62bd34ad0121a2963da01382ac85c4532df2e
M Deepak <M Deepak>
2016-03-26 19:58:35 +0530

    Third commit deepak

--- dam1.js
+++ dam1.js
@@ -10,14 +10,19 @@ Parse.initialize("34olOarYIJtsSy1YcuCdR4RFBPzKthQfAyotWjXP", "vvoqLVt7kMDwuxYliM
 var pulses = 0;
 var lastFlowRateTimer = 0;
 var volume;
-
+var nxtdmpdcff,nxtdmtv;
 //BOARD ON
 var board = new five.Board();
-var TestObject1 = Parse.Object.extend("DAMData1");
+var TestObject1 = Parse.Object.extend("DAMData001");
 var testObject1 = new TestObject1();
-var TestObject2 = Parse.Object.extend("DAMData2");
+var TestObject2 = Parse.Object.extend("DAMData002");
 var testObject2 = new TestObject2();
 
+var TestObjecttv1 = Parse.Object.extend("DAMDatatv001");
+var testObjecttv1 = new TestObjecttv1();
+
+var TestObjecttv2 = Parse.Object.extend("DAMDatatv002");
+var testObjecttv2 = new TestObjecttv2();
 
 
 
@@ -125,6 +130,8 @@ testObject1.set("waterlevel", waterlevel);
 testObject1.set("rainwater",rainwater);
 testObject1.set("waterflow", waterflow);
 testObject1.set("predctncff", predctncff);
+//testObject1.set("token", "1");
+
 testObject1.save(null, {
   success: function(testObject1) {
     // Execute any logic that should take place after the object is saved.
@@ -146,56 +153,74 @@ console.log(predctncff);
 
 if (predctncff>=threshold)
 {
-          var dischargewater=tdl-threshold;
+          var dischargewater=predctncff-threshold;
           var dischargewaterps=dischargewater/300;
-          var query = new Parse.Query(testObject1);
+          var query = new Parse.Query(testObject2);
           query.descending("createdAt");
           query.limit(1);
           query.include("post");
+          query.include("post1");
           query.find({
-          success: function(testObject1) {
+          success: function(testObject2) {
               var post = testObject2[0].get("predctncff");
               var post1 = testObject2[0].get("threshold");
-              var nxtdmpdcff=post;
-              var nxtdmtv = post1;
+              console.log("token passing001");
+
+               nxtdmpdcff=post;
+               nxtdmtv = post1;
+          //    console.log("token passing"+nxtdmpdcff);
+
             console.log(nxtdmpdcff);
             console.log(nxtdmtv);
-            if(nxtdmpdcff + dw > nxtdmtv)
-            {
-            testObject2.set("token", "1");
-            testObject2.set("tokenv",dw);
-            }
-            else {
-              realse();
-            }
-          }
-        });
-}
 
-var query = new Parse.Query(testObject1);
-query.descending("createdAt");
-query.limit(1);
-query.include("post");
-query.find({
-success: function(testObject1) {
-        var post = testObject1[0].get("tokenv");
-        var post1 = testObject1[0].get("token");
+          }
+
+        });
+        if(nxtdmpdcff + dischargewater > nxtdmtv)
+        {
+          console.log("...........................................................................................................");
+          testObjecttv2.save({ token:"1",tokenv:dischargewater }).then(function(object) {
+            console.log("yay! it worked");
+          });
+
+
+        }
+        else {
+          // realse();
+        }
+
+}
+//console.log("eMNAKWEDFKJ");
+var query1 = new Parse.Query(testObject1);
+query1.descending("createdAt");
+query1.limit(1);
+query1.include("post");
+query1.find({
+success: function(testObjecttv1) {
+  //console.log("THORA DA NAAAYE23");
+
+        var post = testObjecttv1[0].get("tokenv");
+        var post1 = testObjecttv1[0].get("token");
         var tkn=token;
         var tknvl = post1;
-
+    //    console.log("THORA DA NAAAYE1JHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+//tkn==1;
+console.log(tkn);
 if(tkn==1)
 {
+  //console.log("THORA DA NAAAYE");
           if (tknvl+predctncff>threshold)
           {
-                    dw=tknvl+Waterlevel;
-                    var dw=tdl-threshold;
+
+                    //dw=tknvl+Waterlevel;
+                    var dw=(tknvl+predctncff)-threshold;
                     var dws=dw/300;
-                    var query = new Parse.Query(testObject1);
+                    var query = new Parse.Query(testObject2);
                     query.descending("createdAt");
                     query.limit(1);
                     query.include("post");
                     query.find({
-                    success: function(testObject1) {
+                    success: function(testObject2) {
                         var post = testObject2[0].get("predctncff");
                         var post1 = testObject2[0].get("threshold");
                         var nxtdmpdcff=post;
@@ -204,8 +229,19 @@ if(tkn==1)
                       console.log(nxtdmtv);
                       if(nxtdmpdcff + dw > nxtdmtv)
                       {
-                      testObject2.set("token", "1");
-                      testObject2.set("tokenv",dw);
+                      testObjecttv2.set("token", "1");
+                      testObjecttv2.set("tokenv",dw);
+                      testObjecttv2.save(null, {
+                                    success: function(testObjecttv2) {
+                                      // Execute any logic that should take place after the object is saved.
+                                      alert('New object created with objectId: ' + gameScore.id);
+                                    },
+                                    error: function(testObjecttv2, error) {
+                                      // Execute any logic that should take place if the save fails.
+                                      // error is a Parse.Error with an error code and message.
+                                      alert('Failed to create new object, with error code: ' + error.message);
+                                    }
+                                  });
                       }
                       else {
                         realse();
@@ -217,4 +253,49 @@ if(tkn==1)
   }
 }
 });
+}
+
+function realse(){
+
+
+  var five = require("johnny-five"),
+    board = new five.Board();
+
+  board.on("ready", function() {
+    var servo = new five.Servo({
+      pin: 10,
+      startAt: 90
+    });
+    var lap = 0;
+
+    servo.sweep().on("sweep:full", function() {
+      console.log("lap", ++lap);
+
+      if (lap === 1) {
+        this.sweep({
+          range: [40, 140],
+          step: 10
+        });
+      }
+
+      if (lap === 2) {
+        this.sweep({
+          range: [60, 120],
+          step: 5
+        });
+      }
+
+      if (lap === 3) {
+        this.sweep({
+          range: [80, 100],
+          step: 1
+        });
+      }
+
+      if (lap === 5) {
+        process.exit(0);
+      }
+    });
+  });
+
 }
